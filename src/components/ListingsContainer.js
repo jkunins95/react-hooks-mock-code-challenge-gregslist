dad9f65c8476039dfc005345543719a0dc06bbea
+ <jkunins95@gmail.com>
2023-11-30 15:27:40 -0500

    completed first deliverable, when page loads all of the listings are available for user to see

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,25 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState, useEffect } from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then(resp => resp.json())
+    .then(list => setListings(list))
+  }, [])
+
+  console.log(listings)
+
+  const listingCards = listings.map(listingObj => {
+    return <ListingCard key={listingObj.id} listing={listingObj} />
+  })
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingCards}
       </ul>
     </main>
   );
